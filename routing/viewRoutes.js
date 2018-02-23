e9decb836d96345f9fa8b88cf1fc4718e7aeee06
William Blake <willblakebooking@gmail.com>
2018-02-23 13:22:05 -0600

    adding learnMore.handlebars to directory

--- routing/viewRoutes.js
+++ routing/viewRoutes.js
@@ -3,4 +3,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.render("index");
   });
+  app.get("/learnMore", function (req, res) {
+    res.render("learnMore");
+  });
 };
