90bc33674cc1cff3a97e701ac6d6c09b4157b238
William Blake <willblakebooking@gmail.com>
2018-02-23 15:39:22 -0600

    some routing worked out

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,7 +1,14 @@
 module.exports = function (app) {
 
-    app.get('/api/learnMore', function (req, res) {
-        res.render("learnMore");
+    app.get('/api/tents', function (req, res) {
+
+    });
+    app.get('/api/sleepingBags', function (req, res) {
+
+    });
+    app.get('/api/backpacks', function (req, res) {
+
     });
 
-}
+
+};
