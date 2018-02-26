c0ea7069769cfdad08fc7583af722fed3c4d692d
William Blake <willblakebooking@gmail.com>
2018-02-26 12:03:02 -0600

    fixed button functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-  $("#learn").on("click", function() {
-    alert('Hello World');
-  });
 
-  $("#clearance").on("click", function() {
-    console.log("Hello World");
-  });
-});
