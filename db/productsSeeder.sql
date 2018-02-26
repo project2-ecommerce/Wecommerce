6ff516b5884ba2140bea8d0083ed98aa98cb7a35
Pete Garcia <petegarcia54@sbcglobal.net>
2018-02-26 16:33:51 -0600

    data base work in progress

--- db/productsSeeder.sql
+++ db/productsSeeder.sql
@@ -1,2 +1,4 @@
-INSERT INTO products ()
-VALUES ();
+INSERT INTO wecommerce.products (name, category, price, description, image)
+VALUES ('tent1', 'tent', 400, 'test description', 'testImg','test','test');
+-- INSERT INTO products ()
+-- VALUES ();
